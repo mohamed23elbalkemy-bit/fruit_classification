b9b521268773817f62fd2d4daae2ed6626465087
Mohamed Nasr <mohamed23elbalkemy@gmail.com>
2026-01-31 23:50:57 +0200

    implement upload screen

---
+++ lib/ui/screens/upload/upload_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class UploadImageScreen extends StatelessWidget {
+  const UploadImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3FFF6),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFFF3FFF6),
+        leading: IconButton(
+          icon: const Icon(Icons.close, color: Colors.green),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text(
+          "Upload Fruit Image",
+          style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+        ),
+
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Colors.green.withOpacity(0.4),
+                  style: BorderStyle.solid,
+                ),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.green.withOpacity(0.15),
+                    child: const Icon(Icons.upload, color: Colors.green, size: 30),
+                  ),
+                  const SizedBox(height: 25),
+
+                  const Text(
+                    "Drop your image here",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  const Text(
+                    "or click the button below to browse",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+
+                  const SizedBox(height: 26),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding: const EdgeInsets.symmetric(vertical: 14),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.photo_size_select_actual_outlined,color: Colors.white,),
+                          SizedBox(width: 6,),
+                          const Text("Choose from Gallery",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 26),
+
+                  const Text(
+                    "Supports: JPG, PNG, WEBP",
+                    style: TextStyle(color: Colors.grey, fontSize: 12,),
+                  ),
+                  SizedBox(height: 18,)
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 35),
+
+            /// TIPS
+            const Text(
+              "Tips for best results",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            const Tip("Use clear, well-lit photos of the fruit"),
+            const SizedBox(height: 4),
+            const Tip("Center the fruit in the frame"),
+            const SizedBox(height: 4),
+            const Tip("Avoid blurry or dark images"),
+            const SizedBox(height: 4),
+            const Tip("Show the full fruit without obstructions"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Tip extends StatelessWidget {
+  final String text;
+  const Tip(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 6),
+      child: Row(
+        children: [
+          const Icon(Icons.check_circle, color: Colors.green, size: 18),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              text,
+              style: const TextStyle(color: Colors.black54),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
